3a74d54ebca251477ead0acdf5556990fd2bd18b
Rafid Mahbub <92277868+MrCurse@users.noreply.github.com>
2021-12-13 11:29:50 -0600

    Add files via upload

--- mukhanov_sasaki_class.py
+++ mukhanov_sasaki_class.py
@@ -1,28 +1,35 @@
 import numpy as np
-#import numba 
+from numba import float64
+from numba.experimental import jitclass
 
+spec = [ ('N', float64[:]), 
+         ('a', float64[:]), 
+         ('phi', float64[:]), 
+         ('dphi', float64[:]), 
+         ('V', float64[:]) ]
+
+@jitclass(spec)
 class mukhanov_sasaki:
     '''
     Python class for numerically solving the Mukhanov-Sasaki equation beyond slow-roll
     approximations. 
 
     Input parameters:
-    k - comoving wavenumber of perturbation mode (scalar)
     N - efolds (array)
     a - scale factor (array)
     phi - background evolution of inflaton (array)
     dphi - background evolution of inflaton conjugate momentum (array)
     V - inflaton potential (array)
     '''
-    def __init__(self, k, N, a, phi, dphi, V):
-        self.k = k
+    def __init__(self, N, a, phi, dphi, V):
+     
         self.N = N 
         self.a = a
         self.phi = phi 
         self.dphi = dphi 
         self.V = V
 
-    def efold_bounds(self):
+    def efold_bounds(self, k):
         '''
         This method computes the efold times for which the modes are evolved. Defining
         the comoving horizon to be aH, the sub and superhorizon limits are given by
@@ -35,66 +42,73 @@ class mukhanov_sasaki:
         horizon = self.a*H 
         initial = 100*horizon 
         final = 0.01*horizon 
-        test_in = np.absolute(initial-self.k)
-        test_fin = np.absolute(final-self.k)
+        test_in = np.absolute(initial-k)
+        test_fin = np.absolute(final-k)
 
         Nii = np.where( test_in==np.min(test_in) )[0][0]
         Nff = np.where( test_fin==np.min(test_fin) )[0][0]
 
         return Nii, Nff 
 
-    def mode_evolve(self):
+    def mode_evolve(self, k, eps1, eta, d_eps, d_eta):
         '''
         This method computes the evolution of inflaton modes from Nii to Nff by solving
         the Mukhanov-Sasaki equation using fourth order Runge-Kutta (RK4) method.  
+
+        The derivatives of the Hubble flow parameters are passed as arguments and not 
+        evaluated inside the class since numpy.gradient is not supported by numba. One
+        can explicitly define the derivative by writing a simple forward difference
+        code inside the class if that helps.
         '''
         dN = self.N[1] - self.N[0]
-        eps1 = 0.5*self.dphi**2
-        eps2 = np.gradient(eps1, dN)/eps1 
-        eta = eps1 - 0.5*eps2 
-        d_eps = np.gradient(eps1, dN)
-        d_eta = np.gradient(eta, dN)
-
         H = np.sqrt( self.V/(3-0.5*self.dphi**2) )
 
-        Ni, Nf = self.efold_bounds()
+        Ni, Nf = self.efold_bounds(k)
         n = Nf - Ni + 1
 
         uk = np.zeros(n); duk = np.zeros(n)
         vk = np.zeros(n); dvk = np.zeros(n)
 
         #Bunch-Davies vacuum conditions
-        uk[0] = 1/np.sqrt(2*self.k); duk[0] = 0
-        vk[0] = 0; dvk[0] = -np.sqrt(self.k)/(0.01*np.sqrt(2)*self.k)
+        uk[0] = 1/np.sqrt(2*k); duk[0] = 0
+        vk[0] = 0; dvk[0] = -np.sqrt(k)/(0.01*np.sqrt(2)*k)
+
+        '''
+        This method returns the mode evolution, i.e., all the information of uk and others
+        from Ni to Nf. The code can also be modified to produce simply the final values of
+        uk and others for easier calculation of the power spectrum
+
+        uk, duk, vk, dvk ----> uk[-1], duk[-1], vk[-1], dvk[-1]
+        '''
 
         for i in range(n-1):
 
-            f1 = -(1-eps1[Ni+i])*duk[i] - (self.k**2 /(self.a[Ni+i]*H[Ni+i])**2)*uk[i] - (1+eps1[Ni+i]-eta[Ni+i]) \
+            f1 = -(1-eps1[Ni+i])*duk[i] - (k**2 /(self.a[Ni+i]*H[Ni+i])**2)*uk[i] - (1+eps1[Ni+i]-eta[Ni+i]) \
                 *(eta[Ni+i]-2)*uk[i] + (d_eps[Ni+i]-d_eta[Ni+i])*uk[i]
             F1 = duk[i]
-            f2 = -(1-eps1[Ni+i])*(duk[i]+0.5*f1*dN) - (self.k**2 /(self.a[Ni+i]*H[Ni+i])**2)*(uk[i]+0.5*F1*dN) \
+            f2 = -(1-eps1[Ni+i])*(duk[i]+0.5*f1*dN) - (k**2 /(self.a[Ni+i]*H[Ni+i])**2)*(uk[i]+0.5*F1*dN) \
                 - (1+eps1[Ni+i]-eta[Ni+i])*(eta[Ni+i]-2)*(uk[i]+0.5*F1*dN) + (d_eps[Ni+i]-d_eta[Ni+i])*(uk[i]+0.5*F1*dN)
             F2 = duk[i] + 0.5*f1*dN 
-            f3 = -(1-eps1[Ni+i])*(duk[i]+0.5*f2*dN) - (self.k**2 /(self.a[Ni+i]*H[Ni+i])**2)*(uk[i]+0.5*F2*dN) \
+            f3 = -(1-eps1[Ni+i])*(duk[i]+0.5*f2*dN) - (k**2 /(self.a[Ni+i]*H[Ni+i])**2)*(uk[i]+0.5*F2*dN) \
                 - (1+eps1[Ni+i]-eta[Ni+i])*(eta[Ni+i]-2)*(uk[i]+0.5*F2*dN) + (d_eps[Ni+i]-d_eta[Ni+i])*(uk[i]+0.5*F2*dN)
             F3 = duk[i] + 0.5*f2*dN 
-            f4 = -(1-eps1[Ni+i])*(duk[i]+f3*dN) - (self.k**2 /(self.a[Ni+i]*H[Ni+i])**2)*(uk[i]+F3*dN) \
+            f4 = -(1-eps1[Ni+i])*(duk[i]+f3*dN) - (k**2 /(self.a[Ni+i]*H[Ni+i])**2)*(uk[i]+F3*dN) \
                 - (1+eps1[Ni+i]-eta[Ni+i])*(eta[Ni+i]-2)*(uk[i]+F3*dN) + (d_eps[Ni+i]-d_eta[Ni+i])*(uk[i]+F3*dN)
             F4 = duk[i] + f3*dN
 
             uk[i+1] = uk[i] + dN*(F1 + 2*F2 + 2*F3 + F4)/6
             duk[i+1] = duk[i] + dN*(f1 + 2*f2 + 2*f3 + f4)/6 
 
-            g1 = -(1-eps1[Ni+i])*dvk[i] - (self.k**2 /(self.a[Ni+i]*H[Ni+i])**2)*vk[i] - (1+eps1[Ni+i]-eta[Ni+i]) \
+            g1 = -(1-eps1[Ni+i])*dvk[i] - (k**2 /(self.a[Ni+i]*H[Ni+i])**2)*vk[i] - (1+eps1[Ni+i]-eta[Ni+i]) \
                 *(eta[Ni+i]-2)*vk[i] + (d_eps[Ni+i]-d_eta[Ni+i])*vk[i]
             G1 = dvk[i]
-            g2 = -(1-eps1[Ni+i])*(dvk[i]+0.5*g1*dN) - (self.k**2 /(self.a[Ni+i]*H[Ni+i])**2)*(vk[i]+0.5*G1*dN) \
+            g2 = -(1-eps1[Ni+i])*(dvk[i]+0.5*g1*dN) - (k**2 /(self.a[Ni+i]*H[Ni+i])**2)*(vk[i]+0.5*G1*dN) \
                 - (1+eps1[Ni+i]-eta[Ni+i])*(eta[Ni+i]-2)*(vk[i]+0.5*G1*dN) + (d_eps[Ni+i]-d_eta[Ni+i])*(vk[i]+0.5*G1*dN)
             G2 = dvk[i] + 0.5*g1*dN 
-            g3 = -(1-eps1[Ni+i])*(dvk[i]+0.5*g2*dN) - (self.k**2 /(self.a[Ni+i]*H[Ni+i])**2)*(vk[i]+0.5*G2*dN) \
+            g3 = -(1-eps1[Ni+i])*(dvk[i]+0.5*g2*dN) - (k**2 /(self.a[Ni+i]*H[Ni+i])**2)*(vk[i]+0.5*G2*dN) \
                 - (1+eps1[Ni+i]-eta[Ni+i])*(eta[Ni+i]-2)*(vk[i]+0.5*G2*dN) + (d_eps[Ni+i]-d_eta[Ni+i])*(vk[i]+0.5*G2*dN)
             G3 = dvk[i] + 0.5*g2*dN 
-            g4 = -(1-eps1[Ni+i])*(dvk[i]+g3*dN) - (self.k**2 /(self.a[Ni+i]*H[Ni+i])**2)*(vk[i]+G3*dN) \
+            g4 = -(1-eps1[Ni+i])*(dvk[i]+g3*dN) - (k**2 /(self.a[Ni+i]*H[Ni+i])**2)*(vk[i]+G3*dN) \
                 - (1+eps1[Ni+i]-eta[Ni+i])*(eta[Ni+i]-2)*(vk[i]+G3*dN) + (d_eps[Ni+i]-d_eta[Ni+i])*(vk[i]+G3*dN)
             G4 = dvk[i] + g3*dN
 
--- mukhanov_sasaki_class_test.py
+++ mukhanov_sasaki_class_test.py
@@ -5,6 +5,7 @@ import numpy as np
 from numpy import sqrt, exp
 import time
 import matplotlib.pyplot as plt
+import numba
 
 alpha = sqrt(2./3)
 xi = -0.480964
@@ -13,18 +14,22 @@ V0 = 1.27*10**-9
 
 dN = 0.001
 
+@numba.njit
 def V(x):
     return V0*(1 + xi - exp(-alpha*x) - xi*exp(-beta*x**2))**2
 
+@numba.njit
 def dV(x):
     return 2*V0*(1 + xi - exp(-alpha*x) - xi*exp(-beta*x**2))*(alpha*exp(-alpha*x) \
         + 2*beta*xi*x*exp(-beta*x**2))
 
+@numba.njit
 def ddV(x):
     return 2*V0*(alpha*exp(-alpha*x) + 2*beta*xi*x*exp(-beta*x**2))**2 + 2*V0 \
         *(1 + xi - exp(-alpha*x) - xi*exp(-beta*x**2))*(-(alpha**2)*exp(-alpha*x) \
             + 2*beta*xi*exp(-beta*x**2) - 4*xi*(beta**2)*(x**2)*exp(-beta*x**2))
 
+@numba.njit
 def back_evolve(phi_in, efolds):
     N_test = efolds
     n = int(N_test/dN)
@@ -50,15 +55,21 @@ if __name__ == "__main__":
     a0 = 0.217198
     a = a0*exp(N)
 
+    eps1 = 0.5*dphi_bg**2
+    eps2 = np.gradient(eps1, dN)/eps1 
+    eta = eps1 - 0.5*eps2 
+    d_eps = np.gradient(eps1, dN)
+    d_eta = np.gradient(eta, dN)
+
     #import the class
     from mukhanov_sasaki_class import mukhanov_sasaki
 
     k = 0.05 #mode for which the evolution is computed
-    MS = mukhanov_sasaki(k, N, a, phi_bg, dphi_bg, potential)
-    Nii, Nff = MS.efold_bounds()
+    MS = mukhanov_sasaki(N, a, phi_bg, dphi_bg, potential)
+    Nii, Nff = MS.efold_bounds(k)
     n = Nff - Nii + 1
     N = np.linspace(dN*Nii, dN*Nff, n)
-    uk, duk, vk, dvk = MS.mode_evolve()
+    uk, duk, vk, dvk = MS.mode_evolve(k, eps1, eta, d_eps, d_eta)
 
     '''
     The method mode_evolve() returns the Mukhanov variable. The curvature perturbation
